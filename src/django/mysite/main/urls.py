165ea5bcbfa2a10c869fd92c0d8244aaaf85f7e1
Hun seok SUN <xodidxkq@naver.com>
2023-06-24 17:13:27 +0900

    feat: company_info path 디테일 추가

--- src/django/mysite/main/urls.py
+++ src/django/mysite/main/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("company_list/", views.CompanyList.as_view(), name="company_list"),
     path("search/", views.search_view, name="search"),
-    path("company_info/", views.Companyinfo.as_view(), name="company_info"),
+    path(
+        "company_info/<str:company_name>/",
+        views.Companyinfo.as_view(),
+        name="company_info",
+    ),
 ]
