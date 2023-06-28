19fb8cbc7bda71d060232ce6011ce24bf2f3cd2c
Hun seok SUN <xodidxkq@naver.com>
2023-06-28 13:24:15 +0900

    feat: 재무정보 model 생성

--- src/django/mysite/main/models.py
+++ src/django/mysite/main/models.py
@@ -1,16 +1,33 @@
 from django.db import models
 
 
-# Create your models here.
-class Company(models.Model):
-    Standard_code = models.CharField(max_length=20, verbose_name="표준코드", unique=True)
-    Short_code = models.IntegerField(verbose_name="요약코드")
-    Company_full_name = models.CharField(max_length=256, verbose_name="기업명")
-    Company_name = models.CharField(max_length=256, verbose_name="기업약명")
-
-    def __str__(self):
-        return self.Company_name
+class CisDf(models.Model):
+    corp = models.TextField(blank=True, null=True)
+    account = models.TextField(blank=True, null=True)
+    account_detail = models.TextField(blank=True, null=True)
+    account_detail_2 = models.TextField(blank=True, null=True)
+    number_2018 = models.FloatField(db_column="2018", blank=True, null=True)
+    number_2019 = models.FloatField(db_column="2019", blank=True, null=True)
+    number_2020 = models.FloatField(db_column="2020", blank=True, null=True)
+    number_2021 = models.FloatField(db_column="2021", blank=True, null=True)
+    number_2022 = models.FloatField(db_column="2022", blank=True, null=True)
 
     class Meta:
-        # DataMart Table
+        db_table = "cis_df"
+
+
+class CompanyName(models.Model):
+    standard_code = models.CharField(
+        db_column="Standard_code", unique=True, max_length=20
+    )
+    short_code = models.TextField(db_column="Short_code", blank=True, null=True)
+    company_full_name = models.TextField(
+        db_column="Company_full_name", blank=True, null=True
+    )
+    company_name = models.TextField(db_column="Company_name", blank=True, null=True)
+
+    def __str__(self):
+        return self.company_name
+
+    class Meta:
         db_table = "company_name"
