2b4585d88fcfd20d477d6ebbdc0c544a9a1a768f
devhyunjun <phj5330@gmail.com>
2023-06-28 17:31:33 +0900

    feat: DB 호출하여 bar그래프 시각화

--- src/django/mysite/static/assets/js/chart-bar.js
+++ src/django/mysite/static/assets/js/chart-bar.js
@@ -2,24 +2,35 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
+
+
+function processData(data) {
+  // 여기에서 'data' 변수를 사용하여 원하는 작업을 수행합니다.
+  // 예시로, 'data'를 콘솔에 출력하는 코드를 추가합니다.
+  console.log(data);
+}
+
+// 'processData' 함수를 호출하고 'data' 변수를 전달합니다.
+processData(data);
+
 // Bar Chart Example
 var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["2018", "2019", "2020", "2021", "2022"],
     datasets: [{
       label: "Revenue",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
-      data: [10000, 5312, 6251, 7841, 9821, 14984],
+      data: data,
     }],
   },
   options: {
     scales: {
       xAxes: [{
         time: {
-          unit: 'month'
+          unit: 'year'
         },
         gridLines: {
           display: false
@@ -31,7 +42,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 15000,
+          max: 1000000000000,
           maxTicksLimit: 5
         },
         gridLines: {
