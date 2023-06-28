3da4bfd7f594631e6b348543cd71242897e5e47d
Hun seok SUN <xodidxkq@naver.com>
2023-06-28 13:25:34 +0900

    feat: 재무정보 view 생성

--- src/django/mysite/main/views.py
+++ src/django/mysite/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import FormView, ListView
-from .models import Company
+from .models import CompanyName, CisDf
 
 
 def company_direct(request):
@@ -18,17 +18,28 @@ def home(request):
 
 
 class CompanyList(ListView):
-    model = Company
+    model = CompanyName
     template_name = "company_list.html"
     context_object_name = "company_list"
 
 
 class Companyinfo(ListView):
-    model = Company
+    model = CompanyName
     template_name = "company_info.html"
     context_object_name = "company_info"
 
     def get_queryset(self):
         company_name = self.request.session.get("context")
-        queryset = Company.objects.filter(Company_name=company_name)
+        queryset = CompanyName.objects.filter(company_name=company_name)
+        return queryset
+
+
+class CisDf(ListView):
+    model = CisDf
+    template_name = "company_info.html"
+    context_object_name = "cis_df"
+
+    def get_queryset(self):
+        company_name = self.request.session.get("context")
+        queryset = CisDf.objects.filter(corp=company_name)
         return queryset
